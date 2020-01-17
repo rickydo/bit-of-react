c149c737a72d20b36be708fc469244c6a86eb4ff
Ricky Do <ricky.do@dps.texas.gov>
2020-01-17 08:20:05 -0600

    update with working jest config

---
+++ specs/components/Button.spec.js
@@ -0,0 +1,10 @@
+const Button = require('../../src/components/Button');
+
+
+describe('Button component', () => {
+
+  it('should render without throwing an error', () => {
+    // TODO
+    // expect(shallow(<Button />).find('div').exists().toBe(true));
+  })
+})
